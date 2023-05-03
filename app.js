161b93a4bd653703356d63b249376836065dae1a
Falodun Damilola <dpaulzs11@gmail.com>
2023-05-03 14:28:30 +0100

    dashboard screen

--- app.js
+++ app.js
@@ -1,69 +1,79 @@
+const main = document.querySelector('#main')
+const warrantsearch = document.querySelector('#warrantsearch');
+const ordersearch = document.querySelector('#ordersearch');
+const dashboard = document.querySelector('#dashboard');
+const incidents = document.querySelector('#incidents');
+const profiles = document.querySelector('#profiles');
+const vehicles = document.querySelector('#vehicles');
+const reports = document.querySelector('#reports');
+const misc = document.querySelector('#misc');
+// dashboard selectors
 const warrants = document.querySelector('.warrants');
 const orders = document.querySelector('.orders');
 const checkmarks = document.querySelectorAll('.approve');
 const cancelbuttons = document.querySelectorAll('.reject'); 
-const warrantsearch = document.querySelector('#warrantsearch');
-const ordersearch = document.querySelector('#ordersearch');
 
+const clickLinks = [ dashboard, incidents, profiles, vehicles, reports, misc ];
+// DASHBOARD FUNCTIONALITY
 // WARRANTS DATA
-const warrantsData = [
+let warrantsData = [
     {
-        name: "John Oils",
+        name: "john oils",
         id: '155',
         status: 'pending ',
         imageUrl: 'images/character1.png'
     },
     {
-        name: "James Bond",
+        name: "james bond",
         id: '156',
         status: ' rejected',
         imageUrl: 'images/character.svg'
     },
     {
-        name: "Oliver Twist",
+        name: "oliver twist",
         id: '157',
         status: ' rejected',
         imageUrl: 'images/character2.png'
     },
     {
-        name: "Idris Albak",
+        name: "idris albak",
         id: '158',
         status: ' rejected',
         imageUrl: ''
     },
     {
-        name: "Stoney Christon",
+        name: "stoney christon",
         id: '159',
         status: ' rejected',
         imageUrl: 'images/character1.png'
     },
     {
-        name: "Daniel Greals",
+        name: "daniel greals",
         id: '160',
         status: 'pending ',
         imageUrl: 'images/character.svg'
     },
     {
-        name: "Josh Kennedy",
+        name: "josh kennedy",
         id: '161',
         status: 'pending ',
         imageUrl: 'images/character2.png'
     },
     {
-        name: "Jerry Sheamus",
+        name: "jerry sheamus",
         id: '162',
         status: ' rejected',
         imageUrl: 'images/character.svg'
     },
     {
-        name: "Apollo Creed",
+        name: "apollo creed",
         id: '163',
         status: 'pending ',
         imageUrl: 'images/character1.png'
     },
 ]
 // ORDERS DATA
-const ordersData = [
+let ordersData = [
     {
         name: "John Oils",
         id: '154',
@@ -72,7 +82,57 @@ const ordersData = [
     },
 ]
 
+const pushDashboardContents = () => {
+    main.innerHTML = `
+    <section class="dashboardcontent">
+        <section class="warrants-container">
+            <div class="header">
+                <section class="header-text">
+                    <img src="images/warrants.svg" alt="warrants icon">
+                    <div > 
+                        <p class="bold">WARRANTS <br> <span class="small">WARRANTS</span></p>     
+                    </div>
+                </section>
+    
+                <section class="search-input">
+                    <img src="/images/search-icon.png" alt="search">
+                    <input type="text" name="search" id="warrantsearch" placeholder="Search">
+                </section>
+            </div>
+
+            <section class="warrants">
+                <!-- FUNCTION IN JS HANDLES CONTENT -->
+            </section>
+
+        </section>
+
+        <section class="order-container">
+            <div class="header">
+                <section class="header-text">
+                    <img src="images/orderaccount.svg" alt="order icon">
+                    <div > 
+                        <p class="bold">ORDERS <br> <span class="small">ORDERS</span></p>     
+                    </div>
+                </section>
+    
+                <section class="search-input">
+                    <img src="/images/search-icon.png" alt="search">
+                    <input type="text" name="search" id="ordersearch" placeholder="Search">
+                </section>
+            </div>
+
+            <section class="orders">
+                <!-- FUNCTION IN JS HANDLES CONTENT -->
+            </section>
+        </section>
+    </section>
+    `
+    // pushOrder(orders)
+    // pushWarrant(warrants)
+}
+
 const pushWarrant = (warrantData) => {
+    warrants.innerHTML=''
     warrantData.map((item) => {
         const {name, status, id, imageUrl} = item;
         var statusClass = 'blue'
@@ -102,6 +162,7 @@ const pushWarrant = (warrantData) => {
 }
 
 const pushOrder = (ordersData) => {
+    orders.innerHTML = ''
     ordersData.map((item) => {
         const { name, id, imageUrl } = item;
         orders.innerHTML += `
@@ -117,17 +178,23 @@ const pushOrder = (ordersData) => {
     })
 }
 
+// pushDashboardContents(ordersData, warrantsData);
 pushWarrant(warrantsData);
 pushOrder(ordersData);
 
-warrantsearch.addEventListener('change', (e) => {
-    console.log(e.target.value)
+warrantsearch.addEventListener('input', () => {
+    let searchvalue = warrantsearch.value
+    if(!searchvalue) pushWarrant(warrantsData)
+    let data = warrantsData.filter((warrant) => warrant.name.includes(searchvalue.toLowerCase()));
+    pushWarrant(data)
 })
 
-ordersearch.addEventListener('change', (e) => {
-    console.log(e.target.value)
+ordersearch.addEventListener('input', (e) => {
+    let searchvalue = ordersearch.value
+    if(!searchvalue) pushOrder(ordersData)
+    let data = ordersData.filter((order) => order.name.includes(searchvalue.toLowerCase()));
+    pushOrder(data)
 })
-
 
 checkmarks.forEach((checkmark) => {
     checkmark.addEventListener('click', () => {
