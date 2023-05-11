c7e26974acbd96b6c39b4be95d7fa45b2f67c96a
Falodun Damilola <dpaulzs11@gmail.com>
2023-05-11 04:51:12 +0100

    fixed error and made some corrections

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ clickLinks.map((link) => {
             i.content.classList.remove('show')
             i.content.classList.add('hidden')
             i.tab.classList.remove('side-menu-active')
-            i.tabimage.src = `images/${link.name}tab.svg`
+            i.tabimage.src = `images/${i.name}tab.svg`
         });
         if( link.tab == vehicles ) {
             maincontainer.classList.add('main-container-reverse');
@@ -571,7 +571,11 @@ const pushIncidents = (data) => {
             <h4 class="incident-title">${title}</h4>
             <div class="information">${description} <p class="dot"></p> id: ${id}</div>
             <div class="timebox pointer"> 
-                <p> <img src="images/smalledit.svg" alt="edit"> </p> 
+                <p> 
+                <svg  xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                    <path class="smalledit" d="M8.65 3.4625L6.525 1.3625L7.225 0.6625C7.41667 0.470833 7.65217 0.375 7.9315 0.375C8.21083 0.375 8.44617 0.470833 8.6375 0.6625L9.3375 1.3625C9.52917 1.55417 9.62917 1.7855 9.6375 2.0565C9.64583 2.3275 9.55417 2.55867 9.3625 2.75L8.65 3.4625ZM7.925 4.2L2.625 9.5H0.5V7.375L5.8 2.075L7.925 4.2Z" />
+                </svg>
+                </p> 
                 <p> ${name} - ${time} </p> 
             </div>
         </section>
@@ -637,7 +641,9 @@ const pushReports = (data) => {
             <h4 class="incident-title">${title}</h4>
             <div class="information">${description} <p class="dot"></p> id: ${id}</div>
             <div class="timebox"> 
-                <p> <img src="images/smalledit.svg" alt="edit"> </p> 
+            <svg  xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                <path class="smalledit" d="M8.65 3.4625L6.525 1.3625L7.225 0.6625C7.41667 0.470833 7.65217 0.375 7.9315 0.375C8.21083 0.375 8.44617 0.470833 8.6375 0.6625L9.3375 1.3625C9.52917 1.55417 9.62917 1.7855 9.6375 2.0565C9.64583 2.3275 9.55417 2.55867 9.3625 2.75L8.65 3.4625ZM7.925 4.2L2.625 9.5H0.5V7.375L5.8 2.075L7.925 4.2Z" />
+            </svg>
                 <p> ${name} - ${time} </p> 
             </div>
         </section>
